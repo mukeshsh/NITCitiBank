04d2a6910877793f56cdc3509733d85d00f1570c
TL <TL@gmail.com>
2020-04-12 22:58:45 +0530

    utility.java commited

--- DemoProj1/src/com/nt/beans/Utility.java
+++ DemoProj1/src/com/nt/beans/Utility.java
@@ -3,7 +3,11 @@ package com.nt.beans;
 import java.util.Date;
 
 public class Utility {
+	
 	public Date getSysDate() {
 		return new Date();
+	}
+	public void m1() {
+		System.out.println("Utility m1() method");
 	}
 }
